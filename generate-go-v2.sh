0f85156232b6742a19e0cef86bcfcc61a46a3445
koundinya <koundinya@koundinyas-MacBook-Pro.local>
2024-08-18 00:54:56 +0530

    new

--- generate-go-v2.sh
+++ generate-go-v2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-rm -rf ./generated
-mkdir ./generated
+
 
 protoc --plugin=protoc-gen-go=$(which protoc-gen-go) \
    --plugin=protoc-gen-go-grpc=$(which protoc-gen-go-grpc) \
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module fa-proto
 
-go 1.22.5
+go 1.22
