bf9e03f5fed0f78d7eec4dde999896d3083de2e9
koundinya <koundinya@koundinyas-MacBook-Pro.local>
2024-08-17 21:32:34 +0530

    compiled go

--- generate-go-v2.sh
+++ generate-go-v2.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 rm -rf ./generated
-mkdir generated
+# mkdir generated
 
 protoc --plugin=protoc-gen-go=$(which protoc-gen-go) \
-   --go_out="./generated" \
-   --go-grpc_out="./generated" \
-   --go_opt=paths=source_relative \
-   --go-grpc_opt=paths=source_relative ./protos/**/*.proto
+   --go_out="." \
+   --go-grpc_out="." \
+   ./protos/**/*.proto
+#    --go_opt=paths=source_relative \
+#    --go-grpc_opt=paths=source_relative 
+
